2cbc643c7574525b6115c1b61b0aaf195d65909c
sushant2000 <sushantmawar10@gmail.com>
2021-09-17 11:07:27 +0530

    update

--- A2OJ Ladder/A Leveler/q4.java
+++ A2OJ Ladder/A Leveler/q4.java
@@ -1,12 +1,17 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 public class q4{
     public static Scanner scn = new Scanner(System.in);
 
     public static void equalorNot(String str1 , String str2){
-        if(str1.equals(str2)) System.out.print("0");
-            for(int i = 0; i<str1.length(); i++){
-                if(str1.charAt(i) < str2.charAt(i)) System.out.print("-1");
-                else System.out.print("1");
+        StringBuilder sb = new StringBuilder();
+        String temp1 = str1.toLowerCase();
+        String temp2 = str2.toLowerCase();
+        
+        if(temp1.equals(temp2)) System.out.print("0");
+            for(int i = 0; i < temp1.length(); i++){
+                if(temp1.charAt(i) < temp2.charAt(i)){ System.out.print("-1"); break;}
+                else if (temp1.charAt(i) > temp2.charAt(i)){ System.out.print("1");break;}
         }
     } 
     public static void main(String[] args){
